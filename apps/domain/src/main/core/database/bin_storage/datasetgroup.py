87d44340717411aa06c1962afe10f053a31a7bd0
Ionesio Junior <ionesiojr@gmail.com>
2021-04-01 12:56:35 -0300

    Move JsonObject

--- apps/domain/src/main/core/database/bin_storage/datasetgroup.py
+++ apps/domain/src/main/core/database/bin_storage/datasetgroup.py
@@ -1,6 +1,17 @@
 from .. import BaseModel, db
 
 
+class JsonObject(BaseModel):
+    __bind_key__ = "bin_store"
+    __tablename__ = "json_object"
+
+    id = db.Column(db.String(255), primary_key=True)
+    binary = db.Column(db.JSON())
+
+    def __str__(self):
+        return f"<JsonObject id: {self.id}>"
+
+
 class DatasetGroup(BaseModel):
     __tablename__ = "datasetgroup"
 
