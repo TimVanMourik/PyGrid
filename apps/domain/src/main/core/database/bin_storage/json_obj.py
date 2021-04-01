21fb0560717b3b1bbec4b72ed35234923d63be13
Ionesio Junior <ionesiojr@gmail.com>
2021-04-01 11:36:52 -0300

    ADD string length

--- apps/domain/src/main/core/database/bin_storage/json_obj.py
+++ apps/domain/src/main/core/database/bin_storage/json_obj.py
@@ -5,7 +5,7 @@ class JsonObject(BaseModel):
     __bind_key__ = "bin_store"
     __tablename__ = "json_object"
 
-    id = db.Column(db.String(), primary_key=True)
+    id = db.Column(db.String(255), primary_key=True)
     binary = db.Column(db.JSON())
 
     def __str__(self):
